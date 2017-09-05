f23ee287da28fcdddc0cb44a5dd8125f04590229
jonas wadman <wadmanjonas@gmail.com>
2017-09-05 16:21:55 +0200

    Added API classes

---
+++ tvShowProject/tvShowProject/ApiHandler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace tvShowProject
+{
+    public class ApiHandler
+    {
+        public string GetShowMetaData(string nameOfShow)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://epguides.frecar.no/show/" + nameOfShow + "/info/");
+            string responseString = string.Empty;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+
+                    responseString = reader.ReadToEnd();
+                    reader.Close();
+                    response.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                responseString = exception.Message;
+                //throw;
+            }
+            return responseString;
+        }
+
+        public string GetLatestEpisodeInfo(string nameOfShow)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://epguides.frecar.no/show/" + nameOfShow + "/last/");
+            string responseString = string.Empty;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+
+                    responseString = reader.ReadToEnd();
+                    reader.Close();
+                    response.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                responseString = exception.Message;
+                //throw;
+            }
+            return responseString;
+        }
+
+        public string GetAllEpisodes(string nameOfShow)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://epguides.frecar.no/show/" + nameOfShow);
+            string responseString = string.Empty;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+
+                    responseString = reader.ReadToEnd();
+                    reader.Close();
+                    response.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                responseString = exception.Message;
+                //throw;
+            }
+            return responseString;
+        }
+    }
+}
---
+++ tvShowProject/tvShowProject/Show.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tvShowProject
+{
+    public class Show
+    {
+        public string Title { get; set; }
+        public string Imdb_Id { get; set; }
+        public string Epguide_Name { get; set; }
+    }
+}
