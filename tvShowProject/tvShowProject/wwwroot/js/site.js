3774c1484a6910bb225618abb8db0028197e27a6
Niklas Livfors <niklas.livfors@gmail.com>
2017-09-09 15:43:47 +0200

    Javascript i söktabell och "followed shows"tabell, bild i ShowDetails-vyn.

--- tvShowProject/tvShowProject/wwwroot/js/site.js
+++ tvShowProject/tvShowProject/wwwroot/js/site.js
@@ -1,1 +1,12 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $("td[colspan=2]").find("p").hide();
+    $("table").click(function (event) {
+        event.stopPropagation();
+        var $target = $(event.target);
+        if ($target.closest("td").attr("colspan") > 1) {
+            $target.slideUp();
+        } else {
+            $target.closest("tr").next().find("p").slideToggle();
+        }
+    });
+});
