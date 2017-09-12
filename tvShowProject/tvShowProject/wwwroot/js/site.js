301e577631d15fb719ac251d2c4d4e1cadb8e4c3
Niklas Livfors <niklas.livfors@gmail.com>
2017-09-12 16:37:00 +0200

    Tabell JS

--- tvShowProject/tvShowProject/wwwroot/js/site.js
+++ tvShowProject/tvShowProject/wwwroot/js/site.js
@@ -1,12 +1,28 @@
-﻿$(document).ready(function () {
-    $("td[colspan=2]").find("p").hide();
-    $("table").click(function (event) {
-        event.stopPropagation();
-        var $target = $(event.target);
-        if ($target.closest("td").attr("colspan") > 1) {
-            $target.slideUp();
-        } else {
-            $target.closest("tr").next().find("p").slideToggle();
-        }
-    });
+﻿//$(document).ready(function () {
+//    $("td[colspan=2]").find("p").hide();
+//    $("table").click(function (event) {
+//        event.stopPropagation();
+//        var $target = $(event.target);
+//        if ($target.closest("td").attr("colspan") > 1) {
+//            $target.slideUp();
+//        } else {
+//            $target.closest("tr").next().find("p").slideToggle();
+//        }
+//    });
+//});
+
+$(document).ready('.collapse').on('show.bs.collapse', function () {
+    var groupId = $('#expander').attr('data-group-id');
+    console.log(groupId);
+    if (groupId) {
+        $('#grandparentIcon').html('v');
+    }
+});
+
+$('.collapse').on('hide.bs.collapse', function () {
+    var groupId = $('#expander').attr('data-group-id');
+    console.log(groupId);
+    if (groupId) {
+        $('#' + groupId + 'Icon').html('>');
+    }
 });
